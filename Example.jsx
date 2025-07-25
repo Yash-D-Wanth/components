70debd2f814bdddd16d31526c21c4c338f69c7f2
yashwanth <yashjaiganesh@gmail.com>
2025-07-25 15:32:13 +0530

    counter

--- Example.jsx
+++ Example.jsx
@@ -1,7 +1,28 @@
-function Example(props){
-    let {Person}=props;
+import { useState } from "react"
+function Example(){
+    const [num,setValue]=useState(1);
+            const add=()=>{
+                setValue((num)=>{
+                    return num+=1
+                })
+            }
+            const sub=()=>{
+                setValue((num)=>{
+                    return num-=1
+                })
+            }
+            const reset=()=>{
+                setValue((num)=>{
+                    return num==1
+                })
+            }
     return(
-        <div>orewa {props.Person.name} who will became {props.Person.aim} his age {props.Person.age}</div>
+        <>
+        <h1>{num}</h1>
+        <button onClick={add}>+</button>
+        <button onClick={reset}>reset</button>
+        <button onClick={sub}>-</button>
+        </>
     )
 }
-export default Example
+export default Example  
