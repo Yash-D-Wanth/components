bab6c1b505906a3cae7cbec31557d1a084f70e43
yashwanth <yashjaiganesh@gmail.com>
2025-07-28 20:47:16 +0530

    class components

--- Example.jsx
+++ Example.jsx
@@ -1,17 +1,10 @@
-function Example({ Person }) {
-  //   .message="there"
-  return (
-    <>
-      <h1>
-        Hello, this is an example {Person.name}{" "}
-        <ul>
-          {Person.hobbies.map(hobby => (
-            <li key={hobby}>{hobby}</li>
-          ))}
-        </ul>
-      </h1>
-      <h1>{Person.age}</h1>
-    </>
-  );
+import { Component } from "react";
+// function Example() {
+//   return <h1>yashwanth</h1>;
+// }
+class Example extends Component{
+  render(){
+    return <h1>yashwanth</h1>
+  }
 }
 export default Example;
